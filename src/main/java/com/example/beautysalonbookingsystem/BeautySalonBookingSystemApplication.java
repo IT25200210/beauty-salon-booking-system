aef9e6713ac0c2b833b26f386d772aa98d6b1072
ArachchiYKH <it25100072@my.sliit.lk>
2026-05-06 09:34:23 +0530

    First Commit

---
+++ src/main/java/com/example/beautysalonbookingsystem/BeautySalonBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.beautysalonbookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautySalonBookingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeautySalonBookingSystemApplication.class, args);
+    }
+
+}
